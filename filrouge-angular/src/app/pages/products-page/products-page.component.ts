d3cc4cfa6817ec78257c88176b630282814847d5
Jeremy-Huleux <huleux.jeremy@gmail.com>
2024-02-13 11:26:29 +0100

    update

--- filrouge-angular/src/app/pages/products-page/products-page.component.ts
+++ filrouge-angular/src/app/pages/products-page/products-page.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductListComponent } from "./product-list/product-list.component";
 import Formation from '../../models/formation.model';
-// import Album from '../../models/album.model';
 import { FormationService } from '../../services/formation/formation.service';
-// import { AlbumService } from '../../services/album.service';
 
 @Component({
     selector: 'app-products-page',
@@ -16,13 +14,9 @@ export class ProductsPageComponent {
 
   formations: Formation[] = []
 
-  // albums: Album[] = []
-
-  constructor(private formationService: FormationService) {}//, private albumService: AlbumService
-
+  constructor(private formationService: FormationService) {}
   ngOnInit(): void {
     this.formationService.getFormations().subscribe((formations) => {this.formations = formations;});
-    // this.albumService.getAlbums().subscribe((albums) => {this.albums = albums;});
   }
 
 }
