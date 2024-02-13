99702392462403ce3fb973a4c50ae6e766a8fc95
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2024-02-13 10:20:09 +0100

    update localhost formations

--- filrouge-angular/src/app/services/formation/formation.service.ts
+++ filrouge-angular/src/app/services/formation/formation.service.ts
@@ -8,12 +8,12 @@ import Formation from '../../models/formation.model';
 })
 export class FormationService {
 
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'http://localhost:8080';
 
   constructor(private httpClient: HttpClient) { }
 
   getFormations(): Observable<Formation[]> {
-    return this.httpClient.get<Formation[]>(`${this.apiUrl}/formations`);
+    return this.httpClient.get<Formation[]>(`${this.apiUrl}/formations/all`);
   }
 
   getFormation(id: number): Observable<Formation> {
