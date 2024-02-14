146317e2da1929f31604e6c754d119d4dafeba01
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2024-02-14 15:24:49 +0100

    new dossier

--- filrougeSpring/filrougeSpring/src/main/java/fr/equipefilrouge/filrougeSpring/repository/FormationRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.equipefilrouge.filrougeSpring.repository;
-
-import fr.equipefilrouge.filrougeSpring.entity.Formation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FormationRepository extends JpaRepository<Formation, Long> {
-}
